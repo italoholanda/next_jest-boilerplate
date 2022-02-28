28902e2389096dd78a5934b0c173d67d2feeeb88
italo <you@example.com>
2022-02-28 15:52:45 -0300

    changed button style

--- src/components/Button/style.tsx
+++ src/components/Button/style.tsx
@@ -1,18 +1,18 @@
 import styled from 'styled-components'
 
 export const Button = styled.button`
-  background-color: var(--grey-600);
+  background-color: var(--grey-900);
   color: var(--white);
 
-  border-radius: 0.5rem;
-  border: var(--grey-1000) 1px solid;
+  border-radius: 0.3rem;
+  border: var(--grey-600) 1px solid;
 
   padding: 0.5rem 1rem;
 
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 1rem;
+  gap: 0.5rem;
 
   transition: all 0.3s;
 
