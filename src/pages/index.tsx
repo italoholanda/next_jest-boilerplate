8502f241fa68d4185a76c3d52c979672e54389c8
italo <you@example.com>
2022-02-28 15:54:21 -0300

    working modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import Header from 'components/Header'
+import Modal from 'components/Modal'
+import { useState } from 'react'
 import Main from 'sections/Main'
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
   return (
     <>
-      <Header />
+      <Header setIsModalOpen={setIsModalOpen} />
       <Main />
+      {isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
     </>
   )
 }
